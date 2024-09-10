5acf9b2c06853767c4791d9fada9c5527ea7cd98
JonathanVarghese <jvarghese@bsd220.org>
2024-09-10 09:50:50 -0500

    Part 7 Done

---
+++ iDine/OrderView.swift
@@ -0,0 +1,38 @@
+//
+//  OrderView.swift
+//  iDine
+//
+//  Created by Jonathan V on 9/10/24.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                        HStack {
+                            Text(item.name)
+                            Spacer()
+                            Text("$\(item.price)")
+                        }
+                    }
+                }
+                Section {
+                    NavigationLink("Place Order") {
+                        Text("Check Out")
+                    }
+                }
+            }
+            .navigationTitle("Order")
+        }
+    }
+}
+
+#Preview {
+    OrderView()
+        .environmentObject(Order())
+}
