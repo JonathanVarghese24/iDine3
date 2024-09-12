81d4aa57a70093c6d5668b2ecad0e1a0e9d96a2f
JonathanVarghese <jvarghese@bsd220.org>
2024-09-12 09:41:04 -0500

    Part 8 Done

---
+++ iDine/MainView.swift
@@ -0,0 +1,29 @@
+//
+//  MainView.swift
+//  iDine
+//
+//  Created by Jonathan V on 9/12/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+            
+            OrderView()
+                .tabItem {
+                    Label("Order", systemImage: "square.and.pencil")
+                }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+        .environmentObject(Order())
+}
